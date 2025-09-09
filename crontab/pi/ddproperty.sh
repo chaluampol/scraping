b86292b9bc91a6691b26435436db6212df8a3eac
ppscthk <suravoot03@gmail.com>
2025-09-10 00:25:43 +0700

    edit ddproperty.sh in crontab

--- crontab/pi/ddproperty.sh
+++ crontab/pi/ddproperty.sh
@@ -6,4 +6,4 @@ source myenv/bin/activate
 DATE=$(date +'%Y-%m-%d')
 
 echo "$(date) cron ddproperty is working" >> /home/dev-team/Desktop/scraping/logs/cron.ddproperty-$(date +\%Y-\%m-\%d).log 2>&1
-python ddproperty_v4.py >> "/home/dev-team/Desktop/scraping/logs/cron.ddproperty-${DATE}.log" 2>&1
+python ddproperty_v5.py >> "/home/dev-team/Desktop/scraping/logs/cron.ddproperty-${DATE}.log" 2>&1
